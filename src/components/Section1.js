36a3727f98525fd83002524ed1cc7bcf05dfd5fb
Sapt75 <109506771+Sapt75@users.noreply.github.com>
2022-10-16 13:38:14 +0530

    Sunday 1st

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -6,14 +6,15 @@ const Section1 = () => {
             <div>
                 <h1 className='lg:text-4xl sm:text-2xl xsm:text-xl sx:text-base font-semibold text-center head py-10'>RAKSHAM DEFINES THE MOMENT SAVER </h1>
             </div>
-            <section class="text-gray-600 body-font">
-                <div class="container mx-auto flex md:px-20 md:py-24 sm:py-10 md:flex-row flex-col items-center">
-                    <div class="lg:max-w-ss lg:w-full md:w-1/3 w-4/6 mb-10 md:mb-0">
-                        <img class="object-cover object-center rounded" alt="hero" src="/img_1.jpg" />
+            <section className="text-gray-600 body-font">
+                <div className="container mx-auto flex md:px-10 md:py-24 sm:py-10 md:flex-row flex-col items-center">
+                    <div className="lg:max-w-ss lg:w-full md:w-1/2 w-4/6 mb-10 md:mb-0">
+                        <img className="object-cover object-center rounded" alt="hero" src="/img_1.jpg" />
                     </div>
-                    <div class="lg:flex-grow sx:text-sm md:text-base md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                        <p class="mb-8 leading-relaxed">A PHOTOGRAPH or a CINE-VIDEO everything is more important for memories , when as we offer you both . Not a single thing should be perfect and another will okay okay that's not good for your memories we think. We try to make each and every corner of your memorable day should be perfect. After many years when you look at that PHOTOGRAPHS or at the CINE-VIDEOS that shouldn't looks older, that should be looks modern at that time also . So we try to gather future modern memories that looks now and also after many years looks like a newborn baby.
-                            Atlast, RAKSHAM हैं तो सक्षम हैं। </p>
+                    <div className="lg:flex-grow sx:text-sm md:text-base md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                        <p className="mb-8 leading-relaxed">A PHOTOGRAPH or a CINE-VIDEO everything is more important for memories , when as we offer you both . Not a single thing should be perfect and another will okay okay that's not good for your memories we think. We try to make each and every corner of your memorable day should be perfect. After many years when you look at that PHOTOGRAPHS or at the CINE-VIDEOS that shouldn't looks older, that should be looks modern at that time also . So we try to gather future modern memories that looks now and also after many years looks like a newborn baby.
+                        </p>
+                        <p className="mb-8 leading-relaxed">Atlast, RAKSHAM हैं तो सक्षम हैं। </p>
                     </div>
                 </div>
             </section>
